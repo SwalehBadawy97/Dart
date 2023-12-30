57be34e1b19d1c9e9f799a958a7c6b1466818937
Swaleh Abubakar Badawy <35217854+SwalehBadawy97@users.noreply.github.com>
2023-12-30 12:20:05 +0300

    Add files via upload

---
+++ data types.dart
@@ -0,0 +1,33 @@
+void main() {
+  // example use of the months int
+  print("The num of months in a year is $months");
+
+  //example use of Double
+  print(pi);
+
+  //example use of String.
+  print("My name is $name");
+
+  // example use of list
+  print("Some of the cars in the market are $cars");
+
+  //example use of map
+  print("Your logon details are: $account");
+
+
+}
+
+// Integer. Data type for whole numbers
+int months = 12;
+
+// Double. Data type for floating numbers/decimals
+double pi = 3.14;
+
+// String. Data type for sequence of characters
+String name = "Swaleh";
+
+//List. Data type for collection of ordered items.
+List cars = ["Toyota","Mercedes","Nissan"];
+
+// Map. A collection of key-value pairs
+var account = {'User': 'Admin', 'Pass': 'asdfg'};
